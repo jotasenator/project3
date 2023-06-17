7f3320f4532d465eebfb76b58662587570152555
Maykel <jotasenator@gmail.com>
2023-06-17 03:57:49 -0400

    Implement send email logic

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,33 +1,68 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener( 'DOMContentLoaded', function ()
+{
 
   // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
-  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector( '#inbox' ).addEventListener( 'click', () => load_mailbox( 'inbox' ) );
+  document.querySelector( '#sent' ).addEventListener( 'click', () => load_mailbox( 'sent' ) );
+  document.querySelector( '#archived' ).addEventListener( 'click', () => load_mailbox( 'archive' ) );
+  document.querySelector( '#compose' ).addEventListener( 'click', compose_email );
+
+  //Submit new email form
+  document.querySelector( '#compose-form' ).addEventListener( 'submit', ( e ) =>
+  {
+    e.preventDefault();
+
+    const recipients = document.querySelector( '#compose-recipients' ).value;
+    const subject = document.querySelector( '#compose-subject' ).value;
+    const body = document.querySelector( '#compose-body' ).value;
+
+    fetch( '/emails', {
+      method: 'POST',
+      body: JSON.stringify( {
+        recipients,
+        subject,
+        body,
+      } )
+    } )
+      .then( response => response.json() )
+      .then( result =>
+      {
+
+        // Print result
+        console.log( result );
+
+      } );
+    console.log( { recipients, subject, body } );
+
+    load_mailbox( 'sent' );
+
+  } );
 
   // By default, load the inbox
-  load_mailbox('inbox');
-});
+  load_mailbox( 'inbox' );
+} );
 
-function compose_email() {
+function compose_email ()
+{
 
   // Show compose view and hide other views
-  document.querySelector('#emails-view').style.display = 'none';
-  document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector( '#emails-view' ).style.display = 'none';
+  document.querySelector( '#compose-view' ).style.display = 'block';
 
   // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+  document.querySelector( '#compose-recipients' ).value = '';
+  document.querySelector( '#compose-subject' ).value = '';
+  document.querySelector( '#compose-body' ).value = '';
 }
+//it shows the menu and display whatever the text I placed
+function load_mailbox ( mailbox )
+{
 
-function load_mailbox(mailbox) {
-  
   // Show the mailbox and hide other views
-  document.querySelector('#emails-view').style.display = 'block';
-  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector( '#emails-view' ).style.display = 'block';
+  document.querySelector( '#compose-view' ).style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  document.querySelector( '#emails-view' ).innerHTML = `<h3>${ mailbox.charAt( 0 ).toUpperCase() + mailbox.slice( 1 ) }</h3>`;
 }
+
